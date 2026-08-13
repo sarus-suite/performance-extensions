36ef51111ad670ca5bf43f2043896f7bd474a533
Alberto Madonna <alberto.madonna@cscs.ch>
2026-08-14 00:11:59 +0200

    sethomevar: fix error handling and streamline code

--- crates/sethomevar/src/main.rs
+++ crates/sethomevar/src/main.rs
@@ -54,17 +54,12 @@ fn run() -> Result<()> {
     let mut value = read_stdin_json()?;
     let obj = ensure_obj(value.as_object_mut(), "top-level JSON must be an object")?;
 
-    let replace = match get_replace_mode(obj) {
-        Ok(value) => value,
-        Err(_) => REPLACE_DEFAULT_VALUE,
-    };
-
     let env_entries_raw = vec![get_home_env_entry(obj)?];
 
     // Validate env entries and merge as strings
     let env_entries = validate_env_strings(env_entries_raw)?;
     if !env_entries.is_empty() {
-        merge_process_env_strings(obj, env_entries, replace)?;
+        merge_process_env_strings(obj, env_entries)?;
     }
 
     // Pretty-print output JSON with trailing newline
@@ -90,49 +85,53 @@ fn run() -> Result<()> {
     Ok(())
 }
 
-fn get_replace_mode(obj: &mut Map<String, Value>) -> Result<bool, String> {
+fn get_replace_mode(obj: &Map<String, Value>) -> Result<bool> {
     // Ensure "annotations" exists
-    let annotations_val = obj.entry("annotations".to_string());
-    match annotations_val {
-        Entry::Vacant(_) => return Err(format!("Validation error: 'annotations' doesn't exist.")),
-        Entry::Occupied(_) => {}
-    }
-
-    // Ensure "annotations" is an object
-    let annotations_obj = annotations_val
-        .or_insert_with(|| json!({}))
-        .as_object_mut()
+    let annotations = obj
+        .get("annotations")
         .ok_or_else(|| {
-            "Validation error: 'annotations' exists but is not an object.".to_string()
-        })?;
-
-    // Check REPLACE_ANNOTATION_NAME entry exists
-    let replace_val = annotations_obj.entry(REPLACE_ANNOTATION_NAME.to_string());
-    match replace_val {
-        Entry::Vacant(_) => {
-            return Err(format!(
-                "Validation error: '{}' doesn't exist.",
-                REPLACE_ANNOTATION_NAME
-            ))
-        }
-        Entry::Occupied(_) => {}
-    }
-
-    let replace: &str = replace_val
-        .or_insert_with(|| json!("false"))
-        .as_str()
+            Error::new(
+                ExitStatus::DataErr,
+                "Validation error: 'annotations' doesn't exist.",
+            )
+        })?
+        // Ensure "annotations" is an object
+        .as_object()
         .ok_or_else(|| {
-            format!(
-                "Validation error: 'annotations.{}' exists but is not a string.",
-                REPLACE_ANNOTATION_NAME
+            Error::new(
+                ExitStatus::DataErr,
+                "Validation error: 'annotations' exists but is not an object.",
             )
         })?;
 
-    match replace {
-        "true" => return Ok(true),
-        "false" => return Ok(false),
-        _ => return Ok(REPLACE_DEFAULT_VALUE),
-    }
+    // Check REPLACE_ANNOTATION_NAME entry exists
+    let replace = annotations
+        .get(REPLACE_ANNOTATION_NAME)
+        .ok_or_else(|| {
+            Error::new(
+                ExitStatus::DataErr,
+                format!(
+                    "Validation error: '{}' doesn't exist.",
+                    REPLACE_ANNOTATION_NAME
+                ),
+            )
+        })?
+        .as_str()
+        .ok_or_else(|| {
+            Error::new(
+                ExitStatus::DataErr,
+                format!(
+                    "Validation error: 'annotations.{}' exists but is not a string.",
+                    REPLACE_ANNOTATION_NAME
+                ),
+            )
+        })?;
+
+    Ok(match replace {
+        "true" => true,
+        "false" => false,
+        _ => REPLACE_DEFAULT_VALUE,
+    })
 }
 
 // Return the HOME environment entry from the system account database.
@@ -357,12 +356,10 @@ fn validate_kv_format(s: &str) -> Result<()> {
 // 2. we validate out envs
 // 3 new env entries are added using two rules
 // 3.1 we append if the env var is new
-// 3.2 we replace if we find it duplicated and replace arg is true
-fn merge_process_env_strings(
-    obj: &mut Map<String, Value>,
-    env_entries: Vec<String>,
-    replace: bool,
-) -> Result<(), String> {
+// 3.2 we replace if we find it duplicated and replace annotation is true
+fn merge_process_env_strings(obj: &mut Map<String, Value>, env_entries: Vec<String>) -> Result<()> {
+    let replace = get_replace_mode(obj).unwrap_or(REPLACE_DEFAULT_VALUE);
+
     // Ensure "process" is an object
     let process_val = obj
         .entry("process".to_string())
